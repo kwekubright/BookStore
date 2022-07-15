05f16dfe3ff602cedda7e9bc953989660410dc07
Kweku Bright <kweku01bright@gmail.com>
2022-07-15 17:07:12 +0100

    Style app

--- src/component/Book.js
+++ src/component/Book.js
@@ -1,9 +1,11 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import { useDispatch } from 'react-redux';
+import { CircularProgressbar } from 'react-circular-progressbar';
 import { removeBook, removeBookR } from '../redux/books/books';
 import '../index.css';
 import './book.css';
+import 'react-circular-progressbar/dist/styles.css';
 
 function Book(props) {
   const { bookId, title, author, category, className } = props;
@@ -33,26 +35,38 @@ function Book(props) {
         <div className="actions flex">
           <ul className="flex">
             <li className="color-primary">
-              <button type="button">Comments |</button>
-            </li>
-            <li className="color-primary">
-              <button type="button" onClick={() => handleClick()}>
-                {' '}
-                Remove |
+              <button type="button" className="button-texty">
+                Comments | &nbsp;
               </button>
             </li>
             <li className="color-primary">
-              <button type="button"> Edit</button>
+              <button
+                type="button"
+                className="button-texty"
+                onClick={() => handleClick()}
+              >
+                Remove | &nbsp;
+              </button>
+            </li>
+            <li className="color-primary ">
+              <button type="button" className="button-texty">
+                Edit
+              </button>
             </li>
           </ul>
         </div>
       </div>
-      <div className="flex flex-justify-start book-details flex-gap-5 width-40p">
-        <div className="progress flex flex-column flex-justify-center ">
-          <div className="progress-bar" />
-          <div className="flex flex-column flex-justify-center">
-            <span className="prog-number">50%</span>
-            <span>Completed</span>
+      <div className="flex flex-justify-start book-details flex-gap-5 width-40p  flex-justify-start">
+        <div className="flex flex-justify-center flex-align-start flex-align-center pr-4">
+          <div className="prog-bar">
+            <CircularProgressbar value={50} />
+          </div>
+          <div className="progress flex flex-column flex-justify-center ">
+            <div className="progress-bar" />
+            <div className="flex flex-column flex-justify-center">
+              <span className="prog-number">50%</span>
+              <span>Completed</span>
+            </div>
           </div>
         </div>
         <div className="book-actions flex flex-column flex-justify-space-between">
